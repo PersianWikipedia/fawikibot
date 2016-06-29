87335be6ba1db033202f31095ca6574ba4d569e7
mohammad reza <reza.energy@gmail.com>
2016-06-29 20:19:45 +0430

    Update fa_cosmetic_changes_core.py

--- fa_cosmetic_changes_core.py
+++ fa_cosmetic_changes_core.py
@@ -579,6 +579,7 @@ def UnicodeURL(text,msg=msg):
             try:
                 URL=URL.split('<')[0]
                 new_URL=urllib.unquote(URL.encode('utf8')).decode('utf8').replace(u' ',u'%20').replace(u'{',u'%7B').replace(u'|',u'%7C').replace(u'}',u'%7D').replace(u'[',u'%5B').replace(u']',u'%5D')
+                new_URL=new_URL.replace(u'"',u'%22').replace(u"'",u'%27')
                 new_URL = re.sub('[\r\n\t]', "",new_URL)
                 text=text.replace(URL,new_URL)
             except:
