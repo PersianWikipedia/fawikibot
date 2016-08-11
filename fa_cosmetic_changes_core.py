bde93828061eeef5572e28192359350b245de94f
mohammad reza <reza.energy@gmail.com>
2016-08-11 12:54:01 +0430

    Update fa_cosmetic_changes_core.py

--- fa_cosmetic_changes_core.py
+++ fa_cosmetic_changes_core.py
@@ -419,9 +419,9 @@ def cleaning(text,msg_short,msg=msg):
     text=re.sub(ur'\[\[ *(https?\:\/\/.*?) *\]\]',u'[\1]',text)
     text=re.sub(ur'\[\[ *(\/\/.*?) *\]\]',u'[\1]',text)
     text=re.sub(ur'(https?:\/?\/?){2,}',u'\1',text)
-    text=re.sub(ur'^ +(\=+.*?\=+)',u'\1',text)
+    text=re.sub(ur'^ +(\=+[^\=]+\=+)',u'\1',text, re.MULTILINE)
     text=re.sub(ur'\[\[\|',u'[[',text)
-    text=re.sub(ur'\[\[(.*?)\{\{\!\}\}(.*?)\]\]',u'[[\1|\2]]',text)
+    text=re.sub(ur'\[\[([^\]]+)\{\{\!\}\}([^\]]+)\]\]',u'[[\1|\2]]',text)
     text=re.sub(ur'\[{2}([^\|]+)\|\1\]{2}',u'[[\1]]',text)
 
     #حذف رده انگلیسی    
