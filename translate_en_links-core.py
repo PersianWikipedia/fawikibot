6ecd3fb17f4ade5e8d2ae966c55e328bcf885666
mohammad reza <reza.energy@gmail.com>
2017-01-27 11:11:02 +0330

    rewrite

--- translate_en_links-core.py
+++ translate_en_links-core.py
@@ -14,6 +14,9 @@ import codecs
 from pywikibot import config
 import time
 import MySQLdb as mysqldb
+ensite = pywikibot.Site('en', 'wikipedia')
+fasite = pywikibot.Site('fa', 'wikipedia')
+
 BotVersion=u'۹.۱ core'
 _cache={}
 
@@ -99,19 +102,21 @@ def Compairing(s1,s2):
 def is_equal (s1,s2):
     s1=s1.strip()
     s2=s2.strip()
+    CleanComp=Compairing(clean_word(s1),clean_word(s2))
+    Comp=Compairing(s1,s2)
     if s1.lower() in s2.lower() or s2.lower() in s1.lower():
-      return True 
-    elif Compairing(clean_word(s1),clean_word(s2))> 0.7:
-      return True
-    elif Compairing(s1,s2) > 0.7:
-      return True
+      return True, 100
+    elif CleanComp > 0.5:
+      return True, CleanComp
+    elif  Comp > 0.5:
+      return True, Comp
     else:
-      return False
+      return False, CleanComp
 
 def redirect_find( page_link):
     page_link=page_link.replace(u' ',u'_')
     if _cache.get(tuple([page_link, 'redirect_find'])):
-        return _cache[tuple([page_link, 'redirect_find'])]
+        return _cache[tuple([page_link, 'redirect_find'])], 0
     params = {
         'action': 'query',
         'redirects':"",
@@ -120,20 +125,83 @@ def redirect_find( page_link):
     query_page =pywikibot.data.api.Request(site=pywikibot.Site('en'), **params).submit()
     try:
         redirect_link=query_page[u'query'][u'redirects'][0]['to']
-        if is_equal (page_link,redirect_link):
+        is_equal_result,Comp=is_equal (page_link,redirect_link)
+        if is_equal_result:
             #It is redirect but it seems ok to replace
             _cache[tuple([page_link, 'redirect_find'])]=True
-            return True
+            return True, Comp
         else:
             _cache[tuple([page_link, 'redirect_find'])]=False
-            return False
+            return False, Comp
     except:
         if 'missing=""' in str(query_page):
             _cache[tuple([page_link, 'redirect_find'])]=False
-            return False
+            return False, 0
         else:
             _cache[tuple([page_link, 'redirect_find'])]=True
-            return True
+            return True, 0
+
+
+def link_translator(batch, ensite, fasite):
+    params = {
+        'action': 'query',
+        'redirects': '',
+        'titles': '|'.join(batch)
+    }
+    query_res = pywikibot.data.api.Request(site=ensite, **params).submit()
+    redirects = {i['from']: i['to'] for i in query_res['query'].get('redirects', [])}
+    normalizeds = {i['from']: i['to'] for i in query_res['query'].get('normalized', [])}
+    
+    # resolve normalized redirects and merge normalizeds dict into redirects at the same time
+    for k, v in normalizeds.items():
+        redirects[k] = redirects.get(v, v)
+
+    wikidata = pywikibot.Site('wikidata', 'wikidata')
+    
+    endbName = ensite.dbName()
+    fadbName = fasite.dbName()
+    params = {
+        'action': 'wbgetentities',
+        'sites': endbName,
+        'titles': '|'.join([redirects.get(i, i) for i in batch]),
+        'props': 'sitelinks'
+    }
+
+    try:
+        query_res = pywikibot.data.api.Request(site=wikidata, **params).submit()
+    except:
+        return {}
+    
+    matches_titles = {}
+    entities = query_res.get('entities', {})
+    for qid, entity in entities.items():
+        if fadbName in entity.get('sitelinks', {}):
+            en_title = entity['sitelinks'][endbName]
+            fa_title = entity['sitelinks'][fadbName]
+
+            # for not updated since addition of badges on Wikidata items
+            if not isinstance(en_title, str):
+                en_title = en_title['title']
+                fa_title = fa_title['title']
+
+            matches_titles[en_title] = fa_title
+        
+    res = {}
+    for i in batch:
+        p = redirects[i] if (i in redirects) else i
+        if p in matches_titles:
+            res[i] = matches_titles[p]
+
+    for k, v in redirects.items():
+        if k in res:
+            res[v] = res[k]
+
+    for k, v in normalizeds.items():
+        if k in res:
+            res[v] = res[k]
+        
+    return res
+
 
 def englishdictionry( enlink ,firstsite,secondsite):
     try:
@@ -148,34 +216,16 @@ def englishdictionry( enlink ,firstsite,secondsite):
         return False
     if enlink==u'':
         _cache[tuple([enlink,firstsite, 'englishdictionry'])]=False
-        return False    
-
-    site = pywikibot.Site(firstsite)
-    sitesecond= pywikibot.Site(secondsite)
-    params = {
-        'action': 'query',
-        'prop': 'langlinks',
-        'titles': enlink,
-        'redirects': 1,
-        'lllimit':500,
-    }
-    try:
-        categoryname =pywikibot.data.api.Request(site=site, **params).submit()  
-        for item in categoryname[u'query'][u'pages']:
-            case=categoryname[u'query'][u'pages'][item][u'langlinks']
-        for item in case:
-            if item[u'lang']==secondsite:
-                intersec=item[u'*']
-                break
-        result=intersec
-        if result.find('#')!=-1:
-            _cache[tuple([enlink,firstsite, 'englishdictionry'])]=False
-            return False
-        _cache[tuple([enlink,firstsite, 'englishdictionry'])]=result
-        return result
-    except: 
+        return False   
+    
+    englishdictionry_result=link_translator([enlink], ensite, fasite)
+    if englishdictionry_result:
+        _cache[tuple([enlink,firstsite, 'englishdictionry'])]=englishdictionry_result[enlink]
+        return englishdictionry_result[enlink]
+    else:
         _cache[tuple([enlink,firstsite, 'englishdictionry'])]=False
-        return False
+        return False 
+
 
 def switchnamespace(namespace):
     if namespace==0:
@@ -201,93 +251,93 @@ def revert(text2,text):
     return text2
 
 def getlinks(enlink,falink,NotArticle):
-        site = pywikibot.Site('fa')
-        enlink_2=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', enlink).strip()
-        falink_2=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', falink).strip()
-        if not enlink_2:
-            # It is date linke like [[1999]] which is [[1999]] != [[۱۹۹۹]]
-            return
-        if not falink_2:
-            # It is date linke like [[۱۹۹۹]] which is [[1999]] != [[۱۹۹۹]]
-            return
+
+    site = pywikibot.Site('fa')
+    enlink_2=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', enlink).strip()
+    falink_2=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', falink).strip()
+    if not enlink_2:
+        # It is date linke like [[1999]] which is [[1999]] != [[۱۹۹۹]]
+        return
+    if not falink_2:
+        # It is date linke like [[۱۹۹۹]] which is [[1999]] != [[۱۹۹۹]]
+        return
+    try:
+        page = pywikibot.Page(site,enlink)
+        linktos=page.getReferences()
+    except:
+        return False
+    for page in linktos:
+        namespacefa=page.namespace()
+        if namespacefa in [1,2,3,5,7,8,9,11,13,15,101]:
+            continue
+        if NotArticle:
+            #At the first it should replace at none article
+            if namespacefa==0:    
+                continue
         try:
-            page = pywikibot.Page(site,enlink)
-            linktos=page.getReferences()
-        except:
-            return False
-        for page in linktos:
-                namespacefa=page.namespace()
-                if namespacefa in [1,2,3,5,7,8,9,11,13,15,101]:
-                    continue
-                if NotArticle:
-                    #At the first it should replace at none article
-                    if namespacefa==0:    
-                        continue
-                try:
-                    text=page.get()
-                except :
-                    continue
-                if _cache.get(tuple([page.title(),enlink,'getlinks'])):
-                    continue
-                else:
-                    _cache[tuple([page.title(),enlink,'getlinks'])]=1
-                time.sleep(2)    
-                pywikibot.output(u'checking '+page.title()+u' .....')    
-                enlink=enlink.strip()
-                text2=text
-                fanum=enlink.replace(u'1',u'۱').replace(u'2',u'۲').replace(u'3',u'۳').replace(u'4',u'۴').replace(u'5',u'۵')
-                fanum=fanum.replace(u'6',u'۶').replace(u'7',u'۷').replace(u'8',u'۸').replace(u'9',u'۹').replace(u'0',u'۰')    
-                fanum_text=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', fanum).strip()
-                search_list=[]
+            text=page.get()
+        except :
+            continue
+        if _cache.get(tuple([page.title(),enlink,'getlinks'])):
+            continue
+        else:
+            _cache[tuple([page.title(),enlink,'getlinks'])]=1
+        time.sleep(2)    
+        pywikibot.output(u'checking '+page.title()+u' .....')    
+        enlink=enlink.strip()
+        text2=text
+        fanum=enlink.replace(u'1',u'۱').replace(u'2',u'۲').replace(u'3',u'۳').replace(u'4',u'۴').replace(u'5',u'۵')
+        fanum=fanum.replace(u'6',u'۶').replace(u'7',u'۷').replace(u'8',u'۸').replace(u'9',u'۹').replace(u'0',u'۰')    
+        fanum_text=re.sub(ur'[1234567890۱۲۳۴۵۶۷۸۹۰\(\)\,\،\.]',ur'', fanum).strip()
+        search_list=[]
 
-                if  fanum_text and fanum!=enlink:
-                    for i in [enlink,enlink.lower(),enlink.upper(),enlink.replace(u',',u'،'),enlink.replace(u' ',u'_')]:
-                        if not i in search_list:
-                            search_list.append(i)
-                else:
-                    for i in [enlink,enlink.lower(),enlink.upper(),enlink.replace(u',',u'،'),fanum,enlink.replace(u' ',u'_')]:
-                        if not i in search_list:
-                            search_list.append(i)
+        if  fanum_text and fanum!=enlink:
+            for i in [enlink,enlink.replace(u',',u'،'),enlink.replace(u' ',u'_'),enlink.replace(u' ',u'_').replace(u',',u'،')]:
+                if not i in search_list:
+                    search_list.append(i)
+        else:
+            for i in [enlink,enlink.replace(u',',u'،'),fanum,enlink.replace(u' ',u'_'),enlink.replace(u' ',u'_').replace(u',',u'،')]:
+                if not i in search_list:
+                    search_list.append(i)
 
-                if text.find(enlink)!=-1:
-                    text2=text2.replace(u'\r',u'')
-                    for enlink in search_list:    
-                        text2=re.sub(ur'\[\[ *(:|) *'+enlink+ur' *\]\]',ur'[[\1'+falink+ur']]',text2)
-                        Link_list = re.findall(ur'\[\[ *(:|) *([^\]\|]+) *\| *([^\]\|]+) *\]\]',text, re.S)
-                        if Link_list:
-                            for mylink in Link_list:
-                                if enlink==mylink[1]:
-                                    pywikibot.output(u'link >'+mylink[1]+u' '+ mylink[2])
-                                    Replace_list = re.findall(ur'\[\[( *:|)( *'+enlink+ur' *)\|([^\]\|]+ *)\]\]',text, re.S)
-                                    if Replace_list:
-                                        for replace_link in Replace_list:
-                                            if len(replace_link[2]) <4 and len (enlink)>3:
-                                                text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+ur'|'+replace_link[2]+u']]')                                               
-                                            else:
-                                                if replace_link[2]==re.sub(u'[ابضصثقفغعهخحجچشسیلتنمکگظطزرذدپو]',u'',replace_link[2]):
-                                                    text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+u']]')
-                                                else:
-                                                    text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+ur'|'+replace_link[2]+u']]') 
+        text2=text2.replace(u'\r',u'')
+        for enlink in search_list:
+            pywikibot.output(enlink)
+            text2=re.sub(ur'\[\[ *(:|) *'+enlink+ur' *\]\]',ur'[[\1'+falink+ur']]',text2, re.DOTALL| re.IGNORECASE)
+            Link_list = re.findall(ur'\[\[ *(:|) *([^\]\|]+) *\| *([^\]\|]+) *\]\]',text2, re.DOTALL| re.IGNORECASE)
+            if Link_list:
+                for mylink in Link_list:
+                    if enlink.lower().replace(u'_',u' ').replace(u'،',u',').strip()==mylink[1].lower().replace(u'_',u' ').replace(u'،',u',').strip():
+                        pywikibot.output(u'link >'+mylink[1]+u' '+ mylink[2])
+                        Replace_list = re.findall(ur'\[\[( *:|)( *'+enlink+ur' *)\|([^\]\|]+ *)\]\]',text2, re.DOTALL| re.IGNORECASE)
+                        if Replace_list:
+                            for replace_link in Replace_list:
+                                if len(replace_link[2]) <4 and len (enlink)>3:
+                                    text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+ur'|'+replace_link[2]+u']]')                                               
+                                else:
+                                    if replace_link[2]==re.sub(u'[ابضصثقفغعهخحجچشسیلتنمکگظطزرذدپو]',u'',replace_link[2]):
+                                        text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+u']]')
+                                    else:
+                                        text2=text2.replace(u'[['+replace_link[0]+replace_link[1]+u'|'+replace_link[2]+u']]',ur'[['+replace_link[0]+falink+ur'|'+replace_link[2]+u']]') 
 
-                    if text2.find(falink)==-1:
-                        pywikibot.output(u'\03{lightblue}could not find any link\03{default}')
-                    text2=revert(text2,text)
-                    msg=u''
-                    if text!=text2:
-                        text2,msg=ref_link_correction_core.main(text2,u'')
-                        if msg:
-                           msg=u'+'+msg
-                        try:
-                            page.put(text2,u'ربات :[[وپ:پقا|جایگزینی پیوند قرمز]] [['+enlink+u']] > [['+falink+u']] ('+BotVersion+')'+msg)
-                            pywikibot.output(u'\03{lightgreen}the page '+page.title()+u' had replcae item [['+enlink+u']] > [['+falink+u']]\03{default}')
-                        except:
-                            pywikibot.output(u'\03{lightred}the page '+page.title()+u' could not replaced so it passed\03{default}')
-                            continue
-                else:
-                     pywikibot.output(u'\03{lightred}could not find andy link\03{default}')        
-        return True
+        if text2.find(falink)==-1:
+            pywikibot.output(u'\03{lightblue}could not find any link\03{default}')
+        text2=revert(text2,text)
+        msg=u''
+        if text!=text2:
+            text2,msg=ref_link_correction_core.main(text2,u'')
+            if msg:
+               msg=u'+'+msg
+            try:
+                page.put(text2,u'ربات :[[وپ:پقا|جایگزینی پیوند قرمز]] [['+enlink+u']] > [['+falink+u']] ('+BotVersion+')'+msg)
+                pywikibot.output(u'\03{lightgreen}the page '+page.title()+u' had replcae item [['+enlink+u']] > [['+falink+u']]\03{default}')
+            except:
+                pywikibot.output(u'\03{lightred}the page '+page.title()+u' could not replaced so it passed\03{default}')
+                continue
+       
+    return True
         
-def remove_wikify (enlink,Status):
+def remove_wikify (enlink,Status,Comp):
     try:
         page = pywikibot.Page(pywikibot.Site('fa'),enlink)
         linktos=page.getReferences()
@@ -305,9 +355,9 @@ def remove_wikify (enlink,Status):
             
             try:
                 if Status=='R':
-                    page.put(text,u'[[وپ:پقا|برداشتن ویکی‌سازی]] [['+enlink+u']] > تغییرمسیر نامشابه است ('+BotVersion+')')
+                    page.put(text,u'[[وپ:پقا|برداشتن ویکی‌سازی]] [['+enlink+u']] > تغییرمسیر نامشابه است ('+BotVersion+') '+str(Comp))
                 else:
-                    page.put(text,u'[[وپ:پقا|برداشتن ویکی‌سازی]] [['+enlink+u']]> بخشی از یک مقاله است (در ویکی‌انگلیسی# دارد) ('+BotVersion+')')
+                    page.put(text,u'[[وپ:پقا|برداشتن ویکی‌سازی]] [['+enlink+u']]> بخشی از یک مقاله است (در ویکی‌انگلیسی# دارد) ('+BotVersion+') '+str(Comp))
                 pywikibot.output(u'\03{lightblue}the page '+page.title()+u' remove wikifay [['+enlink+u']]\03{default}')
             except:
                 pywikibot.output(u'\03{lightred}the page '+page.title()+u' could not replaced so it passed\03{default}')
@@ -326,7 +376,6 @@ def get_query():
     cursor.execute(querys)
     results = cursor.fetchall()
     return results
-
     
 def run(results, NotArticle):
     for enlink in results:
@@ -340,12 +389,13 @@ def run(results, NotArticle):
                     count+=1
                     enlink=enlink.replace(i,str(count))
                 #unwikify the redirect links
-                if not redirect_find(enlink):
+                redirect_find_result, Comp =redirect_find(enlink)
+                if not redirect_find_result:
                     pywikibot.output(u'It was redirect so lets remove the wikify!')
-                    remove_wikify (enlink,'R')
+                    remove_wikify (enlink,'R',Comp)
                     continue
-
                 falink=englishdictionry(enlink ,'en','fa')
+                pywikibot.output(falink)
                 if falink:
                     if namespacefinder(enlink ,'en')!=namespacefinder(falink ,'fa'):
                         continue    
@@ -355,7 +405,7 @@ def run(results, NotArticle):
                 else:
                     #unwikify the # links
                     if u'#' in enlink:
-                        remove_wikify (enlink,'#')
+                        remove_wikify (enlink,'#',Comp)
 
     del results,enlink
 
@@ -365,6 +415,7 @@ run(get_query(),True)
 run(get_query(),False)
 '''
 # for test
-results=[['Book',0],['book',0],['computer',0],['Computer',0],['dog',0],['Fars Province',0],['Book#Types of books',0],['Books',0],['ISO 3166-1:IR',0]]
+results=[['100 Mile House, British Columbia',0]]
+run(results,True)
 run(results,False)
 '''
