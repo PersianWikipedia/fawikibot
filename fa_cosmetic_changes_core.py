86370698eb5ce98f6b50f0d73f73866c16c1645f
mohammad reza <reza.energy@gmail.com>
2017-03-07 00:09:01 +0330

    Update fa_cosmetic_changes_core.py

--- fa_cosmetic_changes_core.py
+++ fa_cosmetic_changes_core.py
@@ -25,7 +25,7 @@ pywikibot.config.put_throttle = 0
 pywikibot.config.maxthrottle = 0
 
 testpass=False
-cleaning_version=u'۱۴.۷ core'
+cleaning_version=u'۱۴.۸ core'
 msg=u'('+cleaning_version +u')' 
 faSite=pywikibot.Site('fa',fam='wikipedia')
 _cache = {}
