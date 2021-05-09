60243b08fc705e244aed28d36ba54f11a2c56f5c
Huji <huji.huji@gmail.com>
2021-05-08 21:59:56 -0400

    Adding HujiBot's grid jobs and crontab

---
+++ HujiBot/grid/jobs/daily.sh
@@ -0,0 +1,4 @@
+source ~/venv/bin/activate
+python3 ~/core/pwb.py touch -page:"صفحهٔ_اصلی" -purge
+python3 ~/core/pwb.py hujibot_20
+python3 ~/core/pwb.py weekly-slow 14
---
+++ HujiBot/grid/jobs/findproxy.sh
@@ -0,0 +1,3 @@
+source ~/venv/bin/activate
+python3 ~/core/pwb.py findproxy
+python3 ~/core/pwb.py extendblock
