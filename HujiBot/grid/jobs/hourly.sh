d47f4f5cd1b1381fedc9bf8ff852fe3fb753ab2a
Huji Lee <huji.huji@gmail.com>
2021-08-23 17:18:31 -0400

    Categorize will also check 500 random pages

--- HujiBot/grid/jobs/hourly.sh
+++ HujiBot/grid/jobs/hourly.sh
@@ -3,3 +3,4 @@ python3 ~/core/pwb.py ANBcounter
 python3 ~/core/pwb.py GANtable
 python3 ~/core/pwb.py FACtable
 python3 ~/core/pwb.py categorize -newpages:500 -always
+python3 ~/core/pwb.py categorize -random:500 -always
