fcfb312e55fc3bf4b5fc5f0ba5e89190fead8d02
Huji Lee <huji.huji@gmail.com>
2021-09-14 22:41:22 -0400

    Temporarily turn off categorize.py

--- HujiBot/grid/jobs/hourly.sh
+++ HujiBot/grid/jobs/hourly.sh
@@ -2,5 +2,5 @@ source ~/venv/bin/activate
 python3 ~/core/pwb.py ANBcounter 
 python3 ~/core/pwb.py GANtable
 python3 ~/core/pwb.py FACtable
-python3 ~/core/pwb.py categorize -newpages:500 -always
-python3 ~/core/pwb.py categorize -random:500 -always
+# python3 ~/core/pwb.py categorize -newpages:500 -always
+# python3 ~/core/pwb.py categorize -random:500 -always
