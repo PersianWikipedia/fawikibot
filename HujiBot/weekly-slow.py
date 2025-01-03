c8a6324786c827ca524b28c2b603648347c3be0c
Huji Lee <huji.huji@gmail.com>
2025-01-02 21:31:57 -0500

    Add weekly-slow 21

--- HujiBot/weekly-slow.py
+++ HujiBot/weekly-slow.py
@@ -1180,6 +1180,42 @@ LIMIT 500
             "frmt": "| {{formatnum:%d}} || [[کاربر:%s]] || {{formatnum:%s}}",
             "sign": True,
         },
+        {
+            "sqlnum": 21,
+            "sql": """
+SELECT
+  page_title,
+  COUNT(ll.ll_lang) AS interwiki_count,
+  COALESCE(pp.pp_value, '&mdash;') AS description
+FROM page p
+JOIN langlinks ll
+  ON p.page_id = ll.ll_from
+LEFT JOIN page_props pp
+  ON p.page_id = pp.pp_page
+  AND pp.pp_propname = 'wikibase-shortdesc'
+LEFT JOIN langlinks ll2
+  ON p.page_id = ll2.ll_from
+  AND ll2.ll_lang = 'fa'
+WHERE
+  p.page_namespace = 0
+  AND p.page_is_redirect = 0
+  AND ll2.ll_lang IS NULL
+GROUP BY
+  p.page_title,
+  pp.pp_value
+HAVING COUNT(ll.ll_lang) > 10
+ORDER BY interwiki_count DESC
+LIMIT 100
+""",
+            "out": "ویکی‌پدیا:گزارش دیتابیس/مقاله‌های مهم ایجادنشده بر پایه دیگر میان‌ویکی‌ها",
+            "cols": ["ردیف", "مقاله", "شمار میان‌ویکی‌ها", "توضیحات"],
+            "summary": "به روز کردن آمار",
+            "pref": "[[رده:گزارش‌های دیتابیس ویکی‌پدیا]]\n{{/بالا}}"
+            + "\nآخرین به روز رسانی: ~~~~~",
+            "frmt": "| {{formatnum:%d}} || [[:en:%s]] || {{formatnum:%s}}"
+            + " |dir=ltr| %s",
+            "sign": True,
+        },
     ]
 
     for t in tasks:
