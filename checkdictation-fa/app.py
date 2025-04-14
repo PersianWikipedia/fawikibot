74bcc3ef1649f44f699d43ee03f548f5c8ee9aec
Huji <huji.huji@gmail.com>
2025-04-14 15:51:28 -0400

    Tidy app.py

--- checkdictation-fa/app.py
+++ checkdictation-fa/app.py
@@ -1,22 +1,21 @@
 ﻿#!/usr/bin/env python3
 
-from flask import Flask, request, Response
-import json
+from flask import Flask, Response
 from zz_checkDictation_cmd import run
-
 
 
 app = Flask(__name__)
 
 
-@app.route('/check/<path:page>')
+@app.route("/check/<path:page>")
 def check(page):
-    result = '{}'
+    result = "{}"
     result = run(page)
 
-    response = Response(result, content_type='application/json;charset=utf8')
-    response.headers['Access-Control-Allow-Origin'] = "*"
+    response = Response(result, content_type="application/json;charset=utf8")
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(threaded=True)
